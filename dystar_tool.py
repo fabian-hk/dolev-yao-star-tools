3a110e21abc3dc7b3f36bb6f6f0facdd2239bc1a
Fabian Hauck <contact@fabianhauck.de>
2025-01-22 09:56:27 +0100

    Visualization: Filter states for namespaces; Make Plantuml URL configurable; Use debug events to determine the sender and receiver of a message; Improve visualization

--- dystar_tool.py
+++ dystar_tool.py
@@ -11,29 +11,43 @@ if __name__ == "__main__":
         description="Commandline tool that helps analyzing protocols with DYStar.",
     )
     subparsers = parser.add_subparsers(help="sub-command help")
+
+    # ******* Subparser for Visualization *******
     parser_visualization = subparsers.add_parser(
         "vis",
         help="Parse the output of a DYStar protocol run and generate a Plantuml sequence diagram.",
     )
     parser_visualization.add_argument(
-        "program_path", help="Path to the DYStar program executable."
+        "-u", "--url",
+        help="URL of the Plantuml server (default: https://www.plantuml.com/plantuml/img/)",
+        default="https://www.plantuml.com/plantuml/img/"
+    )
+    parser_visualization.add_argument(
+        "program_path", help="path to the DYStar program executable."
+    )
+    parser_visualization.add_argument(
+        "-n", "--namespaces",
+        help="only print states and events in these namespaces",
+        nargs="*"
     )
     parser_visualization.set_defaults(func=dystar_visualization)
 
+    # ******* Subparser for Validation *******
     parser_validation = subparsers.add_parser(
         "val",
         help="Validate properties of the implementation and the corresponding proofs of a DYStar analysis.",
     )
     parser_validation.add_argument(
-        "folder", help="Path to the folder containing the protocol analysis."
+        "folder", help="path to the folder containing the protocol analysis."
     )
     parser_validation.set_defaults(func=dystar_validation)
 
+    # ******* Subparser for Code Generation *******
     parser_gen = subparsers.add_parser(
         "gen", help="Generate the proof structure for a DYStar analysis."
     )
     parser_gen.add_argument(
-        "folder", help="Path to the folder containing the protocol implementation."
+        "folder", help="path to the folder containing the protocol implementation."
     )
     parser_gen.set_defaults(func=dystar_generate)
 
--- visualization.py
+++ visualization.py
@@ -14,9 +14,14 @@ def find_principals(lines: list[dict]):
                 principals.append(line["Principal"])
     return principals
 
+def preprocess_content(line):
+    line_content = line["Content"]
+    return line_content.replace("<BREAK>", "\\n")
 
 def dystar_visualization(args):
     program_path = args.program_path
+    plantuml_url = args.url
+    namespaces = args.namespaces
     process = subprocess.Popen(
         [program_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
@@ -26,45 +31,55 @@ def dystar_visualization(args):
     # Parse output lines as json and skip lines that are not valid json.
     for line in output.split("\n"):
         try:
-            lines.append(json.loads(line))
-        except json.JSONDecodeError:
+            lines.append(json.loads(line, strict=False))
+        except json.JSONDecodeError as e:
             pass
 
     principals = find_principals(lines)
 
     last_principal = "alice"
-    plantuml = ["@startuml"]
+    receiver = None
+    plantuml = ["@startuml", "skinparam dpi 300", "scale 1000 width"]
     for line in lines:
         # Draw sessions to the plantuml diagram
         if line["Type"] == "Session":
-            plantuml.append(
-                f"{line['Principal']} -> {line['Principal']} : Session {line['Content']}"
-            )
+            if not namespaces or line["Tag"] in namespaces:
+                plantuml.append(
+                    f"\"{line['Principal']}\" -> \"{line['Principal']}\" : Session {preprocess_content(line)}"
+                )
         # Use events to find out at which principal
         # we are right now in the protocol run.
         elif line["Type"] == "Event":
             last_principal = line["Principal"]
+            # Use debug event to determine who the receiver is
+            if line["Tag"] == "DY.Lib.Printing.DebugEvent":
+                debug_info = json.loads(line["Content"])
+                receiver = debug_info["receiver"]
         # Draw messages to the plantuml diagram
         # as arrows from one principal to another
         elif line["Type"] == "Message":
             # We assume that the last active principal is
             # sending the message.
             sender = last_principal
-            # We assume that there are only two principals.
-            # So we can take the other principal as the receiver.
-            # TODO find other heuristics for more complex protocols.
-            principals_copy = principals.copy()
-            principals_copy.remove(sender)
-            receiver = principals_copy[0]
-            plantuml.append(f"{sender} -> {receiver} : {line['Content']}")
+            # If receiver is already known do not use heuristics
+            if not receiver:
+                # We assume that there are only two principals.
+                # So we can take the other principal as the receiver.
+                # TODO find other heuristics for more complex protocols.
+                principals_copy = principals.copy()
+                principals_copy.remove(sender)
+                receiver = principals_copy[0]
+            
+            plantuml.append(f"\"{sender}\" -> \"{receiver}\" : {preprocess_content(line)}")
+            receiver = None
     plantuml.append("@enduml")
 
     # Write generated plantuml code to file
-    # plantuml_file = Path(f"{os.path.basename(program_path)}.puml")
-    # plantuml_file.write_text("\n".join(plantuml))
+    plantuml_file = Path(f"{os.path.basename(program_path)}.puml")
+    plantuml_file.write_text("\n".join(plantuml))
 
     # Generate PNG from plantuml code
-    puml = PlantUML(url="http://www.plantuml.com/plantuml/img/")
+    puml = PlantUML(url=plantuml_url)
     png_raw = puml.processes("\n".join(plantuml))
     png_file = Path(f"{os.path.basename(program_path)}.png")
     png_file.write_bytes(png_raw)
